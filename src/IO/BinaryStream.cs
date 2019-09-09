85b0404bea18a4e7e66c25d02fbaef233600357b
Angelo Breuer <angelobr@outlook.de>
2019-09-09 21:40:51 +0200

    Implemented Write/ReadString in BinaryStream

--- src/IO/BinaryStream.cs
+++ src/IO/BinaryStream.cs
@@ -1,6 +1,7 @@
 ﻿namespace ByteBuffer.IO
 {
     using System;
+    using System.Buffers;
     using System.IO;
     using System.Text;
     using Abstractions;
@@ -15,6 +16,11 @@
         protected readonly byte[] _readBuffer;
         protected readonly byte[] _writeBuffer;
         private readonly bool _leaveOpen;
+
+        /// <summary>
+        ///     The maximum encoded byte size of string.
+        /// </summary>
+        public const int MaximumStringByteSize = 0xFFFF;
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="BinaryStream"/> class.
@@ -192,8 +198,26 @@
         /// <returns>the string read</returns>
         public string ReadString(Encoding encoding)
         {
-            // TODO
-            throw new NotImplementedException();
+            // read the length prefix (a 2-byte long ushort indicating the number of bytes the string has)
+            var byteCount = ReadUShort();
+
+            // rent a buffer that can hold the string
+            var pooledBuffer = ArrayPool<byte>.Shared.Rent(byteCount);
+
+            // ensure the pooled buffer is returned to the pool even if an exception is thrown
+            try
+            {
+                // read data
+                ReadBytes(pooledBuffer, byteCount);
+
+                // decode string
+                return encoding.GetString(pooledBuffer, index: 0, byteCount);
+            }
+            finally
+            {
+                // release / return the buffer to the array pool
+                ArrayPool<byte>.Shared.Return(pooledBuffer);
+            }
         }
 
         /// <summary>
@@ -558,8 +582,36 @@
         /// </exception>
         public int Write(string value, int charIndex, int charCount, Encoding encoding)
         {
-            // TODO
-            throw new NotImplementedException();
+            // rent a buffer that can hold the string and the 2-byte ushort length prefix
+            var pooledBuffer = ArrayPool<byte>.Shared.Rent(encoding.GetMaxByteCount(charCount) + 2);
+
+            // ensure the pooled buffer is returned to the pool even if an exception is thrown
+            try
+            {
+                // encode the string starting at buffer offset 2
+                var length = encoding.GetBytes(value, charIndex, charCount, pooledBuffer, byteIndex: 2);
+
+                // ensure the length does not overflow
+                if (length > MaximumStringByteSize)
+                {
+                    throw new ArgumentException($"The specified string overflows the maximum " +
+                        $"encoded byte length ({MaximumStringByteSize}, 0xFFFF)", nameof(value));
+                }
+
+                // encode length prefix
+                pooledBuffer[0] = (byte)(length >> 8);
+                pooledBuffer[1] = (byte)(length & 0xFF);
+
+                // write buffer
+                Write(pooledBuffer, offset: 0, length + 2);
+
+                return length + 2;
+            }
+            finally
+            {
+                // release / return the buffer to the array pool
+                ArrayPool<byte>.Shared.Return(pooledBuffer);
+            }
         }
 
         /// <summary>
